86a1ba01cc1475e0c92e0937fa08c08f8ed65007
Evdemid <docdemidenkoe@gmail.com>
2020-07-02 12:55:15 -0400

    Step #2 is done

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 
+/*
+Step 1: Create Players
+
+For this game, you will need **two players**.
+
+Each player should have a "name", "ship count", and "game board" which means you will need to use an object. Below are the default values for each object property:
+
+| Property  | Type   | Default Value                                                    |
+| --------- | ------ | ---------------------------------------------------------------- |
+| name      | String | User-provided via the `prompt()` function                        |
+| shipCount | Number | `4` (the number of ships each player starts with)                |
+| gameBoard | Array  | 2-dimensional array with 4 rows and 4 columns, filled with zeros |
+*/
+
+
 let player1 = {
   name: "",
-  shipCount: 4,
+  shipCount: 0,
   gameBoard: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 };
 let player2 = {
   name: "",
-  shipCount: 4,
+  shipCount: 0,
   gameBoard: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 };
 
@@ -28,6 +43,24 @@ The ships should be placed randomly, which means you will need to do the followi
 
 Here's an example of how a player's board would look in visual form:
 */
+
+for (let i = 0; i < 4; i++) {
+  let x = Math.floor(Math.random() * 4);
+  let y = Math.floor(Math.random() * 4);
+  player1.shipCount++;
+  if (player1.gameBoard[x][y] === 1) {
+    continue;
+  }
+}
+
+for (let i = 0; i < 4; i++) {
+  let x = Math.floor(Math.random() * 4);
+  let y = Math.floor(Math.random() * 4);
+  player2.shipCount++;
+  if (player2.gameBoard[x][y] === 1) {
+    continue;
+  }
+}
 
 
 
