660b1462f697d6a3ccd8af592c1d2e72e8c8a780
Evdemid <docdemidenkoe@gmail.com>
2020-07-05 07:36:59 -0400

    Minor changes in step #2

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 
-/*
-Step 1: Create Players
-
-For this game, you will need **two players**.
-
-Each player should have a "name", "ship count", and "game board" which means you will need to use an object. Below are the default values for each object property:
-
-| Property  | Type   | Default Value                                                    |
-| --------- | ------ | ---------------------------------------------------------------- |
-| name      | String | User-provided via the `prompt()` function                        |
-| shipCount | Number | `4` (the number of ships each player starts with)                |
-| gameBoard | Array  | 2-dimensional array with 4 rows and 4 columns, filled with zeros |
-*/
-
-
 let player1 = {
   name: "",
   shipCount: 0,
@@ -28,47 +13,91 @@ let player2 = {
 player1.name = prompt("Type your name");
 player2.name = prompt("Type your name");
 
-/*
-## Step 2: Randomly Add Ships to each Board
 
-Each player's board will start with **four ships**.
 
-The ships should be placed randomly, which means you will need to do the following steps for each player's board:
-
-1. Create a loop that runs until 4 ships have been added to the board
-2. Inside the loop, generate a random `x` and a random `y` coordinate (must be between 0 and 3)
-3. Check if the board space (array element) at those coordinates has a ship or not:
-   - If not, "add a ship" (change the value from `0` to `1`) and increment the total added ships counter
-   - If yes, let the loop continue (do nothing)
-
-Here's an example of how a player's board would look in visual form:
-*/
+alert("Your ships are putting on alert...");
 
 for (let i = 0; i < 4; i++) {
   let x = Math.floor(Math.random() * 4);
   let y = Math.floor(Math.random() * 4);
-  player1.shipCount++;
-  if (player1.gameBoard[x][y] === 1) {
+    if (player1.gameBoard[x][y] === 1) {
     continue;
   }
+  player1.shipCount++;
+  player1.gameBoard[x][y] = 1;
 }
+
+
+
 
 for (let i = 0; i < 4; i++) {
   let x = Math.floor(Math.random() * 4);
   let y = Math.floor(Math.random() * 4);
   player2.shipCount++;
+  player2.gameBoard[x][y] = 1;
   if (player2.gameBoard[x][y] === 1) {
     continue;
   }
+  player2.shipCount++;
+  player2.gameBoard[x][y] = 1;
 }
+console.log(player1.gameBoard, player2.gameBoard);
+/*
+Step 3: Start the Game Play
+
+You will need to loop through the following steps, switching between players 1 and 2, until one of the players wins.
+
+### Step 3a: Ask the Player to Choose Strike Coordinates
+
+Using the `prompt()` function, ask the current player to choose an `x` and `y` coordinate to strike. You will need to store the user's input in a variable(s) for the next step.
+
+### Step 3b: Determine if the Player Sunk their Opponent's Ship
+
+Using the `x` and `y` coordinates from the previous step, check the opponent's board to see if the space (array element) at those indices is a ship (is equal to `1`).
+
+- If yes, "sink the ship" (change the value from `1` to `0`), decrement the opponent's ship count, and show an alert that says "Hit!"
+- If not, show an alert that says "Miss!" and let the loop continue (do nothing)
 
 
+### Step 3c: Check if the Opponent has Any Ships Left
 
+Having decremented the opponent's ship count, check if the number is still greater than zero.
 
+- If not, the game has ended and the current player won
+- If yes, let the loop continue (do nothing) and switch to the other player
+*/
 
+  let getPlayer2X;
+  let getPlayer2Y;
+  let getPlayer1X;
+  let getPlayer1Y;
+  while (player1.shipCount !== 0 && player2.shipCount !== 0)
+    do {
+      let getPlayer2X = prompt("Choose your `x` coordinate to strike.");//#1 player's turn
+      let getPlayer2Y = prompt("Choose your `y` coordinate to strike.");//#1 player's turn
 
+      if (player2.gameBoard[getPlayer2X][getPlayer2Y] == 1) {
+        player2.gameBoard[getPlayer2X][getPlayer2Y] = 0;
+        player2.shipCount--;
+        alert("Hit!");
+      }
+      else {
+        alert("Miss!!");
+      }
+      let getPlayer1X = prompt("Choose your `x` coordinate to strike.");//#2 player's turn
+      let getPlayer1Y = prompt("Choose your `y` coordinate to strike.");//#2 player's turn
 
-const battleship = () => {
+      if (player1.gameBoard[getPlayer1X][getPlayer1Y] == 1) {
+        player1.gameBoard[getPlayer1X][getPlayer1Y] = 0;
+        player1.shipCount--;
+        alert("Hit!");
+      }
+      else {
+        alert("Miss!!");
+    }
+      
+      /*  
+      const battleship = () => {
   return 'The winner is...?'
 }
 
@@ -76,3 +105,4 @@ const gameResult = battleship()
 
 const htmlTarget = document.getElementById('result')
 htmlTarget.innerHTML = gameResult
+*/
